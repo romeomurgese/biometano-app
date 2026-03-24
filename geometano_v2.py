6de9b83eada5affa909e61f02a04dc9696c13748
romeomurgese <117920066+romeomurgese@users.noreply.github.com>
2026-03-24 13:06:50 +0100

    Update geometano_v2.py

--- geometano_v2.py
+++ geometano_v2.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1o0uwVDeEJxGYtfHxpNSCIQzuonMfQzMT
 """
 
-#!pip install streamlit folium streamlit-folium geopy openpyxl
+
 
 # Commented out IPython magic to ensure Python compatibility.
 # %%writefile app.py
@@ -91,6 +91,3 @@ Original file is located at
 # st.write("### 📊 Tabella")
 # st.dataframe(df_filtrato, height=400)
 
-#!streamlit run app.py &>/dev/null &
-
-#!curl ipv4.icanhazip.com
